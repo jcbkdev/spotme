61a949a732c13f1a67de8f7974b3cf7c54112f92
stil-jcbk <kubkon134@gmail.com>
2024-03-27 12:19:48 +0100

    playlist view prototype

--- src/scripts/player/player.js
+++ src/scripts/player/player.js
@@ -1,3 +1,44 @@
+//text width check
+const songInfo = document.getElementById("song-info")
+const songTitle = document.getElementById("title")
+const songAuthor = document.getElementById("author")
+
+const songInfoObserver = new MutationObserver((mutations) => {
+  for (mutation of mutations){
+    function checkWidth(element, width, className){
+      const elementWidth = element.clientWidth;
+      console.log(elementWidth > width)
+      if(elementWidth > width){
+        element.classList.add(className)
+      }else{
+        element.classList.remove(className)
+      }
+    }
+    const element = mutation.target.parentElement;
+    const titleMaxWidth = 270;
+    const authorMaxWidth = 215;
+    switch (element.id) {
+      case "title":
+        checkWidth(element, titleMaxWidth, "too-long")
+        break;
+      case "author":
+        checkWidth(element, authorMaxWidth, "too-long")
+        break;
+      default:
+        break;
+    }
+  }
+})
+
+// const config = {
+
+// }
+
+songInfoObserver.observe(songTitle, {childList: true, subtree: true, characterData: true})
+// songInfoObserver.observe(songAuthor, config)
+
+
+//button functionality
 const playBtn = document.getElementById("play-btn")
 const playBtnIcon = document.getElementById("play-btn-icon")
 
---
+++ src/scripts/playlist/playlist.js
@@ -0,0 +1,5 @@
+const backToPlay = () => {
+	window.open("../view/play.html", "_self");
+};
+
+const loadSongs = () => {};
